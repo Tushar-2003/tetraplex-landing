22cfab309f83c491bc17b5ac6401916ffa8d4779
Tushar-2003 <tushargupta3002@gmail.com>
2023-11-21 21:42:22 +0530

    updated and fixed

--- script.js
+++ script.js
@@ -1,41 +1,95 @@
 
-// navbar logic
+//navbar logic
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menuItems = document.querySelector('.menu-items');
-
+// 
 hamburgerMenu.addEventListener('click', () => {
   menuItems.classList.toggle('show');
 });
 
-// features logic
+//features logic
 
 const featuresDiplay = document.querySelectorAll('.features');
 const featureImages = document.querySelectorAll('.feature-display-image');
 const featureTitles = document.querySelectorAll('.feature-display-title');
 const featureDescriptions = document.querySelectorAll('.feature-display-desc');
-
-
+const veild = document.querySelector('.feature-1');
+const karma = document.querySelector('.feature-2');
+const persona = document.querySelector('.feature-3');
+const concierge = document.querySelector('.feature-4');
+ 
 const features = {
   veild: {
     title: 'Veild',
-    description: 'Veild is a psudo anonymous, and privacy-focused Chat Mechanizm to promote discussion of ideas.',
-    image: '../assets/veild.svg'
+    description: 'Veild is a psudo anonymous, and privacy-focused Chat Mechanism to promote discussion of ideas in a civil manner.',
+    image: '/assets/veild.svg'
   },
   karma: {
     title: 'Karma',
-    description: 'Karma is a decentralized, peer-to-peer, and pseudo anonymous social media platform.',
-    image: '../assets/karma.svg'
+    description: 'karma is a point system to moderate users and promote good behaviour.',
+    image: '/assets/karma.svg'
   },
   persona: {
     title: 'Persona',
-    description: 'Persona is a decentralized, peer-to-peer, and pseudo anonymous social media platform.',
-    image: '../assets/persona.svg'
+    description: 'Persona is a way to create a unique identity for yourself, be anyone you want and be anonymous.',
+    image: '/assets/persona.svg'
   },
   concierge: {
     title: 'Concierge',
-    description: 'Concierge is a decentralized, peer-to-peer, and pseudo anonymous social media platform.',
-    image: '../assets/concierge.svg'
+    description: 'Concierge is a way to get help from our AI assistant, and get your questions answered regarding anything on the platform.',
+    image: '/assets/concierge.svg'
   },
 }
 
-console.log(features.veild.description)
+const updateFeature = (feature) => {
+  feature.addEventListener('click', () => {
+    featureImages.forEach(image => {
+      image.src = features[feature.dataset.feature].image;
+    });
+    featureTitles.forEach(title => {
+      title.innerHTML = features[feature.dataset.feature].title;
+    });
+    featureDescriptions.forEach(description => {
+      description.innerHTML = features[feature.dataset.feature].description;
+    });
+  }
+)
+}
+
+updateFeature(veild);
+updateFeature(karma);
+updateFeature(persona);
+
+function updateFeatureDetails(featureName) {
+  const feature = features[featureName];
+  
+  featureImages.forEach(image => {
+    image.src = feature.image;
+  });
+  
+  featureTitles.forEach(title => {
+    title.innerHTML = feature.title;
+  });
+  
+  featureDescriptions.forEach(description => {
+    description.innerHTML = feature.description;
+  });
+}
+
+veild.addEventListener('click', () => {
+  updateFeatureDetails('veild');
+});
+
+karma.addEventListener('click', () => {
+  updateFeatureDetails('karma');
+});
+
+persona.addEventListener('click', () => {
+  updateFeatureDetails('persona');
+});
+
+concierge.addEventListener('click', () => {
+  updateFeatureDetails('concierge');
+});
+
+ 
